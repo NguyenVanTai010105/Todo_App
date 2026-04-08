8ba539a8cdec40f6ef1bdb8834b8f8782b149164
NguyenVanTai010105 <nvt112005@gmail.com>
2026-04-08 16:16:08 +0700

    Releases project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,18 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { fileURLToPath } from "url"; // 1. Import thêm cái này
+
+// 2. Định nghĩa __dirname cho môi trường ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"), // Bây giờ __dirname sẽ hoạt động
     },
   },
 });
