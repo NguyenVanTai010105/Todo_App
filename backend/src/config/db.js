baed5e1537861c74948fb03544d0bbb8c96f773f
NguyenVanTai010105 <nvt112005@gmail.com>
2026-04-08 15:59:41 +0700

    Releases project

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+    await mongoose.connect(process.env.MONGODB_CONNECTIONSTRING);
     console.log("Lk csdl thành công!");
   } catch (err) {
     console.log("Lỗi khi kn csdl");
